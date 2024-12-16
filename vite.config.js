cc85a96991e05cdf18beb6afe3c755d64c4769a6
jnettli <jonas.nettli@gmail.com>
2024-12-16 05:06:42 +0100

    fix: netlify issue fix attempt 2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '',
+        },
+      ],
+    }),
+  ],
   build: {
     rollupOptions: {
       input: {
@@ -14,5 +25,6 @@ export default defineConfig({
         profileEdit: resolve(__dirname, 'profile/edit/index.html'),
       },
     },
+    outDir: 'dist',
   },
-})
+});
